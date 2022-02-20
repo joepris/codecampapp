fb8b01e985daab6bc672de126f21284098286743
joepris <calpito.jp@gmail.com>
2022-02-20 12:23:26 +0800

    fixed error "clas sName" to "className"

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header() {
 
 	return (
 		<div className="padT4 padB4">
-			<div clas sName="container mobile-container">
+			<div className="container mobile-container">
 				<div className="d-flex justify-content-between">
 					<div>
 						<img alt="SVCC Home Page" src="/images/SVCCLogo.png" />
